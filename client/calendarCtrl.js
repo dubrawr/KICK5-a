a066ca7f9d59a296b10c964f20304f566d4cb547
Deb <deborah.tsao@gmail.com>
2016-10-19 23:09:26 -0700

    starting to get the dates for a hangout

--- client/calendarCtrl.js
+++ client/calendarCtrl.js
@@ -1,5 +1,5 @@
-angular.module('myApp').controller('calendarController', ['$routeParams', '$scope', '$http', 
-function($routeParams, $scope, $http){
+angular.module('myApp').controller('calendarController', ['$routeParams', '$scope', '$http', 'moment', 
+function($routeParams, $scope, $http, moment){
 	console.log($routeParams);
 	$scope.grabInfo = function(){
 		console.log('hello');
@@ -11,7 +11,20 @@ function($routeParams, $scope, $http){
 			method: 'GET',
 			params: request
 			}).then(function(response){
-			console.log(response.data);
+				$scope.data = response.data[0];
+				var startDate = moment(response.data[0].startDate);
+				var endDate = moment(response.data[0].endDate);
+				$scope.dateRange = endDate.diff(startDate, 'days');
+				$scope.days = [];
+				console.log($scope.dateRange);
+				for (i=0; i<= $scope.dateRange; i++){
+					var addDay = moment(response.data[0].startDate).add(i, 'days');
+					$scope.days.push(addDay);
+					
+				}
+				console.log($scope.days);
+
+
 		});
 	};
 
