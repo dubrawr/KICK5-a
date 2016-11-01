2fc91ca8b342d568077fb40f214c63ba2298ebb7
Deb <deborah.tsao@gmail.com>
2016-11-01 15:43:30 -0700

    Trying to add mlab lol

---
+++ config.js
@@ -0,0 +1,6 @@
+exports.DATABASE_URL = process.env.DATABASE_URL ||
+                       global.DATABASE_URL ||
+                       (process.env.NODE_ENV === 'production' ?
+                            'mongodb://deb:lolpassword@ds061365.mlab.com:61365/mean' :
+                            'mongodb://deb:lolpassword@ds061365.mlab.com:61365/mean');
+exports.PORT = process.env.PORT || 3000;
